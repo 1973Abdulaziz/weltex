ac28291ef20f50685a61d8ac125bdf6be6b03ee4
1973Abdulaziz <abdulazizshoqulov@yandex.ru>
2022-09-04 20:29:59 +0500

    weltex

---
+++ js/script.js
@@ -0,0 +1,141 @@
+$(document).ready(function(){
+    $('.button').click(function(){
+        $('ul').toggleClass('show');
+        $('site__logo').toggleClass('show')
+    });
+});
+
+let button1=document.getElementById('button1');
+let call=document.getElementById('call');
+let X=document.getElementById('X');
+
+button1.addEventListener(`click`,function(){
+    call.style.display=`block`;
+});
+X.addEventListener(`click`,function(){
+    call.style.display=`none`;
+});
+
+
+// integratsiya 
+let prodaje1=document.getElementById('prodaje1');
+let text30=document.getElementById('text30');
+let x=document.getElementById('x');
+
+prodaje1.addEventListener(`click`,function(){
+    text30.style.display=`block`;
+});
+x.addEventListener(`click`,function(){
+    text30.style.display=`none`;
+});
+
+//  2chisi
+
+let prodaje2=document.getElementById('prodaje2');
+let text28=document.getElementById('text28');
+let x1=document.getElementById('x1');
+
+prodaje2.addEventListener(`click`,function(){
+    text28.style.display=`block`;
+});
+x1.addEventListener(`click`,function(){
+    text28.style.display=`none`;
+});
+
+// 3chisi
+
+let prodaje3=document.getElementById('prodaje3');
+let text27=document.getElementById('text27');
+let x2=document.getElementById('x2');
+
+prodaje3.addEventListener(`click`,function(){
+    text27.style.display=`block`;
+});
+x2.addEventListener(`click`,function(){
+    text27.style.display=`none`;
+});
+
+// 4 chisi
+let prodaje4=document.getElementById('prodaje4');
+let text26=document.getElementById('text26');
+let x3=document.getElementById('x3')
+
+prodaje4.addEventListener(`click`,function(){
+    text26.style.display=`block`;
+});
+x3.addEventListener(`click`,function(){
+    text26.style.display=`none`;
+});
+
+// 5 chisi
+
+let prodaje5=document.getElementById('prodaje5');
+let text23=document.getElementById('text23');
+let x4=document.getElementById('x4')
+
+prodaje5.addEventListener(`click`,function(){
+    text23.style.display=`block`;
+});
+x4.addEventListener(`click`,function(){
+    text23.style.display=`none`;
+});
+
+// 6 chisi
+
+let prodaje6=document.getElementById('prodaje6');
+let text24=document.getElementById('text24');
+let x5=document.getElementById('x5')
+
+prodaje6.addEventListener(`click`,function(){
+    text24.style.display=`block`;
+});
+x5.addEventListener(`click`,function(){
+    text24.style.display=`none`;
+});
+
+// 7chisi
+
+let prodaje7=document.getElementById('prodaje7');
+let text22=document.getElementById('text22');
+let x8=document.getElementById('x8')
+
+prodaje7.addEventListener(`click`,function(){
+    text22.style.display=`block`;
+});
+x8.addEventListener(`click`,function(){
+    text22.style.display=`none`;
+});
+
+// 8chisi
+
+let prodaje8=document.getElementById('prodaje8');
+let text21=document.getElementById('text21');
+let x9=document.getElementById('x9')
+
+prodaje8.addEventListener(`click`,function(){
+    text21.style.display=`block`;
+});
+x9.addEventListener(`click`,function(){
+    text21.style.display=`none`;
+});
+
+// 9 chisi
+
+let prodaje9=document.getElementById('prodaje9');
+let text20=document.getElementById('text20');
+let x10=document.getElementById('x10')
+
+prodaje9.addEventListener(`click`,function(){
+    text20.style.display=`block`;
+});
+x10.addEventListener(`click`,function(){
+    text20.style.display=`none`;
+});
+
+
+
+
+
+
+
+
